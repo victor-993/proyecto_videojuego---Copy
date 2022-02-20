e4d728bd9996a53af6f4826790ad5d6a1c33a0e3
Victor Hernandez <v.hernandez@cgiar.org>
2022-02-20 17:20:40 -0500

    .

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
   },
   module: {
     rules: [
