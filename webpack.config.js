7da85c2a733da717b9c1a4acb647fef133d27be0
Victor Hernandez <v.hernandez@cgiar.org>
2022-02-20 17:35:16 -0500

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const indextInput = './src/index.html';
-const indexOutput = 'index.html';
 
 module.exports = {
-  mode: 'production',
+  mode: 'none',
   entry: './src/index.js',
   output: {
     filename: 'index.js',
@@ -12,36 +10,12 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'assets/images/',
-              publicPath: 'assets/images/',
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(mp3)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'assets/audio/',
-              publicPath: 'assets/audio/',
-            },
-          },
-        ],
-      },
     ],
   },
   plugins: [
     new HTMLWebpackPlugin({
-      filename: indexOutput,
-      template: indextInput,
+      filename: 'index.html',
+      template: './src/index.html',
     }),
   ],
 };
